f351cf0418916a2afe18d0e03598c06a40f660d3
George Dobbs <george.dobbs@gmail.com>
2022-06-09 16:36:43 -0400

    use argparse.FileType

--- dance/util/sort_vba.py
+++ dance/util/sort_vba.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python
 '''takes an input file with vba source code and puts the functions and subs into alpha order and writes to the output file'''
 import argparse
-from os import path
 
-def existing_file(fn):
-  if path.exists(fn):
-    return fn
-  return None
-
-def process(in_file,out_file):
-  try:
-    out=open(out_file,'w')
-  except Exception:
-    print(f'Cannot open output file {out_file}')
-  with open(in_file,'r') as input:
-    lines=input.readlines()
+def process(input,output):
+  '''
+  Read and process the input stream and send it to the output.
+  The in and out files are already open - they are of type _io.TextIOWrapper
+  They are closed at the end.
+  '''
   keys=['Function','Sub']
   in_header=True
   in_item=False
   header=[]
   code_bits=dict()
-  for line in lines:
+  for line in input.readlines():
     if in_item:
       code_rows+=[line]
       if line.strip().startswith('End'):
@@ -37,20 +30,21 @@ def process(in_file,out_file):
       header+=line
   sorted_code_bits={key: val for key, val in sorted(code_bits.items(), key = lambda ele: ele[0].lower())}
   for line in header:
-    out.write(line)
+    output.write(line)
   for name,val in sorted_code_bits.items():
     print(name)
-    out.write('\n')
+    output.write('\n')
     for line in val['code_rows']:
-      out.write(line)
-  out.close()
+      output.write(line)
+  for fn in input,output:
+    fn.close()
 
   
 
 if __name__ == "__main__":
   # execute only if run as a script
   parser = argparse.ArgumentParser(description ="takes an input file with vba source code and puts the functions and subs into alpha order and writes to the output file")
-  parser.add_argument('in_file', help='provide the name of the input file',type=existing_file)
-  parser.add_argument('out_file', help='provide the name of the output file')
+  parser.add_argument('in_file',type=argparse.FileType('r'), help='provide the name of the input file')
+  parser.add_argument('out_file', type=argparse.FileType('w',encoding='UTF-8'),help='provide the name of the output file')
   args=parser.parse_args()
   process(args.in_file,args.out_file)
