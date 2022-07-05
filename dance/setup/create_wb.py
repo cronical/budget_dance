db10c5744e7083309da8dccfeeb7c9a8f14cbadf
George Dobbs <george.dobbs@gmail.com>
2022-07-05 11:54:11 -0400

    rebuilt setup account load with Pandas

--- dance/setup/create_wb.py
+++ dance/setup/create_wb.py
@@ -1,53 +1,51 @@
 #!/usr/bin/env python
 '''Initialize the workbook'''
 import argparse
-from ast import Store
 from os import chdir,getcwd ,mkdir,walk
 from os.path import exists, join,sep
 from shutil import copy2,rmtree
 from sys import exit
-import xml.etree.ElementTree as ET
 import zipfile
 from openpyxl import Workbook
 from dance.util.logs import get_logger
 from dance.setup.setup_tabs import refresh_sheets
 
 def get_all_file_paths(directory):
-    '''zip does one file at a time so get the list'''
-    # initializing empty file paths list
-    file_paths = []
-  
-    # crawling through directory and subdirectories
-    for root, _, files in walk(directory):
-        for filename in files:
-            # join the two strings in order to form the full filepath.
-            filepath = join(root, filename)
-            file_paths.append(filepath)
-  
-    # returning all file paths
-    return file_paths        
-  
-def zip_up(zip_name,directory): 
+  '''zip does one file at a time so get the list'''
+  # initializing empty file paths list
+  file_paths = []
+
+  # crawling through directory and subdirectories
+  for root, _, files in walk(directory):
+    for filename in files:
+      # join the two strings in order to form the full filepath.
+      filepath = join(root, filename)
+      file_paths.append(filepath)
+
+  # returning all file paths
+  return file_paths
+
+def zip_up(zip_name,directory):
   '''put a directory into a zipfile'''
   dir=getcwd()
   chdir(directory)
-  file_paths = get_all_file_paths("./")
+  file_paths = get_all_file_paths('./')
   # writing files to a zipfile
   with zipfile.ZipFile(dir+sep+zip_name,'w') as zip:
     for file in file_paths:
       zip.write(file)
   chdir(dir)
-  
+
 def create(filename,overwrite=False):
   '''create the excel file, then insert the vba project'''
   logger=get_logger(__file__)
-  logger.info(f'current working directory is {getcwd()}')
+  logger.info('current working directory is {}'.format(getcwd()))
   if not overwrite:
-    logger.error(f'File name {filename} already exists, use -o to force overwrite')
+    logger.error('File name {} already exists, use -o to force overwrite'.format(filename))
     exit(-1)
   wb=Workbook()
   wb.save(filename)
-  logger.info(f'initial file saved as {filename}')
+  logger.info('initial file saved as {}'.format(filename))
   if exists('./tmp'):
     rmtree('./tmp')
   with zipfile.ZipFile(filename, 'r') as z:
@@ -61,21 +59,21 @@ def create(filename,overwrite=False):
   for dst in to_copy:
     src='./vba/'+dst.split(sep)[-1]
     copy2(src,dst)
-    logger.info(f'copied {src} to {dst}')
-  
+    logger.info('copied {} to {}'.format(src,dst))
+
   zip_up(filename,'tmp')
-  logger.info(f're-wrote {filename}')
+  logger.info('re-wrote {}'.format(filename))
   #cleanup
   tmp='./tmp'
   rmtree(tmp)
   mkdir(tmp)
-  logger.info(f'cleaned up {tmp}')
+  logger.info('cleaned up {}'.format(tmp))
 
   refresh_sheets(args.out_file)
 
 if __name__=='__main__':
     # execute only if run as a script
-  parser = argparse.ArgumentParser(description ="initialize the forecast spreadsheet")
+  parser = argparse.ArgumentParser(description ='initialize the forecast spreadsheet')
   parser.add_argument('out_file', help='provide the name of the output file')
   parser.add_argument('-o','--overwrite',default=False, action='store_true',help='force overwrite if file already exists')
   args=parser.parse_args()
--- dance/setup/local_data.py
+++ dance/setup/local_data.py
@@ -1,73 +1,128 @@
 '''get annual various local data'''
+import pandas as pd
+from dance.util.files import tsv_to_df
 
-types={'Assets':'A','Bank Accounts':'B','Credit Cards':'C','Investment Accounts':'I','Liabilities':'L','Loans':'N'}
 def read_data(data_info):
-  groups=data_info['group']
-  skip_type_totals=set(types.keys())-set(groups)
-  no_details_for=data_info['no_details_for']
-  gtotal=[g+' - Total' for g in groups]
-  data=[]
-  names={} # keep track of account names, in order to manage name collision between account name and type
-  # defaults for gains/losses percentages
-  rlz=0
-  unrlz=1
-  with open(data_info['path']) as f:
-    for line in f:
-      if '@' in line:
-        continue # skip the securities
-      row=line.split('\t')
-      # set the type if it is a new major heading
-      if row[0]in types.keys() and len(row[1])==0:
-        group=row[0]
-        type_=types[group]
-        continue
-       # skip securities (and probably currencies but I don't care)
-      if len(row)!=3:
-        continue
-      # skip the headings
-      if row[2]=='Notes\n' or row[0]=='':
-        continue
-      # infer inactive if zero balance
-      active=1 
-      if '$ 0.00' in row[1]:
-        active=0
-      actl_source='=@[Account]' # formula for when it is a type or individual account
-      actl_source_tab='tbl_tranfers_actl' # unless its an investment
-      # keep any group totals
-      if row[0] in gtotal:
-        name=row[0].split(' - ')[0]
-        if name not in types:
-          actl_source+='& " - TOTAL"' # formula when its an account (with subaccounts)
-        if name in names: # occurs if an account has same name as the category 
-          # amend the formula for actual
-          data[names[name]][6]='=@[Account] & " - TOTAL"' 
-        else:
-          data.append([name,type_,1,active,rlz,unrlz,actl_source,actl_source_tab])
-          names[name]=len(data)-1
-        continue
-      # skip the details if we are grouping them together or expect them to be in sub-totals
-      if group in groups + no_details_for:
-        continue
-      if type_ == 'I':
-        parts=row[0].split(' - ')
-        # skip over the cash value, in favor of the total
-        if parts[-1] != 'Total':
-          continue
-        row[0]=' - '.join(parts[:-1])
-        actl_source_tab='tbl_invest_actl' # unless its an investment
+  if data_info['type']== 'md_acct':
+    df= read_accounts(data_info)
+    df= prepare_account_tab(data_info,df)
+    return df
 
-      # don't keep totals from non specified groups
-      if row[0]in skip_type_totals:
-        continue
-      # drop zero balance (non group) rows unless called for 
-      if active==0:
-        if row[0] not in data_info['include_zeros']:
-          continue
-        else:
-          actl_source=None
-          actl_source_tab=None
-      taxable=int(not any([a in row[0] for a in data_info['tax_free_keys']]))
-      data.append([row[0],type_,taxable,active,rlz,unrlz,actl_source,actl_source_tab])
-      names[row[0]]=len(data)-1
-  data=sorted(data,key=lambda x: (x[1],x[0]))
-  return data
+def filter_nz(df,include_zeros):
+  '''get a filter for the the rows in a dataframe that have a balance or are allowed by config.
+
+  args:
+    df: a dataframe with fields, Current Balance and Account
+    include_zeros: a list of account names that should be kept even if zero
+
+  returns: boolean series, where True indicates item should be kept.
+  '''
+  nz=df['Current Balance']!=0
+  za=df['Account'].apply(lambda x: x in include_zeros)
+  nzza=pd.concat([nz, za],axis=1)
+  return nzza.any(axis=1)
+
+# -----------------------------
+def read_accounts(data_info):
+  '''Get the account data as specified in the data_info.
+
+  Depending on the data_info may take items at a total level or leaf level.
+
+  args:
+    data_info: a dictionary with the path of the source file, and three lists:
+    1. group - identifies categories or total levels (with subaccounts)
+    2. no_details_for - identifies categories that are covered by total levels
+    3. include_zeros - indicates accounts that are wanted even though they are zero balance (otherwise ignored)
+
+  returns: dataframe including 'Account', 'Type', 'Current Balance' and 'total_type'
+  '''
+  # doing the same with data frame
+  types={'Assets':'A','Bank Accounts':'B','Credit Cards':'C','Investment Accounts':'I','Liabilities':'L','Loans':'N'}
+  groups=data_info['group']
+  no_details_for=data_info['no_details_for']
+  include_zeros=set(data_info['include_zeros'])
+  include_zeros.update(groups)# add groups to the include zeros list
+  g_totals=[g+' - Total' for g in groups]
+
+  df=tsv_to_df(data_info['path'],skiprows=3)
+  df=df.dropna(how='any',subset='Account')
+
+  # establish category for each row
+  df['category']=''
+  cat='Bank Accounts'# should be in the first row, but this keeps it clean
+  for ix,row in df.iterrows():
+    if pd.isnull(row['Notes']):
+      cat=row['Account']
+      df.loc[ix,'Notes']='' # to allow selecting securities later
+    df.loc[ix,'category']=cat
+
+  #df=df.loc[~ df['Notes'].str.contains('@')] # remove securities / currencies (?)
+
+  # build a boolean grid to track where each account is handled
+  df.reset_index(drop=True,inplace=True)
+  for col in ['ignore','by_total','keep']:
+    df[col]=False
+
+  # distinguish between category and sub-account totals to handle name conflicts
+  df['total_type']='leaf'
+  tot_rows=df['Account'].str.contains(' - Total')
+  i_tots=pd.concat([tot_rows,df['category']=='Investment Accounts'],axis=1).all(axis=1)
+  i_totals=list(set(df.loc[i_tots,'Account'])-{'Investment Accounts - Total'})
+  for ix,row in df.loc[tot_rows,['Account']].iterrows():
+    acct_total_key=row['Account']
+    acct_key=' - '.join(acct_total_key.split(' - ')[:-1])
+    # mark the heading and the total lines
+    sel=df['Account'].apply(lambda x: x in [acct_key , acct_total_key])
+    if sum(sel)==2: # the normal case
+      df.loc[sel,'total_type']=['sub_account','category'][acct_key in types]
+    if sum(sel)==4: # name conflict between category and sub-account total
+      ixs= df.loc[sel].index.tolist()
+      for y in ixs:
+        df.loc[y,'total_type']=['sub_account','category'][y in [ixs[0],ixs[-1]]]
+      sel=pd.concat([sel,df['total_type']=='sub_account'],axis=1).all(axis=1) # only include the sub_account variant
+      assert sum(sel)==2, 'too many name conflicts on {}'.format(acct_key)
+
+    # now that there is no name conflict, mark rows
+    start,end=tuple(df.loc[sel,[]].index)
+    df.loc[[start],'ignore']=True # no further use for headings
+    ixs=range(start+1,end)
+    if acct_total_key in g_totals+i_totals:
+      df.loc[ixs,'keep']=False # leaf nodes covered by a total
+      df.loc[end,'Account']=acct_key
+      df.loc[[end],'keep']=filter_nz(df.loc[[end]],include_zeros)
+      pass
+    else:
+      if acct_key not in no_details_for:
+        sel2=filter_nz(df.loc[ixs],include_zeros)
+        df.loc[ixs,'keep']=sel2# the leaf nodes we will keep unless we have a total at a higher level, in which case, they will later be overwritten
+        df.loc[end+1,'keep']=False # totals we are ignoring
+
+  df=df.loc[df.keep]
+  df['Type']=[types[x] for x in df.category.tolist()]
+  return df[['Account','Type','Current Balance','total_type']]
+
+def prepare_account_tab(data_info, in_df):
+  '''Add in the added fields
+  args:
+    data_info: a dictionary describing the data (from the config.yaml file)
+    in_df: a dataframe with columns: Account, Type, Current Balance, and total_type
+
+  returns: a dataframe for the accounts tab
+  '''
+  df=in_df
+  tax_free_keys=[]
+  if 'tax_free_keys'in data_info:
+    tax_free_keys=data_info['tax_free_keys']
+  account_names=df.Account.tolist()
+  df['Income Txbl']= [ int(not any(y in x for y in tax_free_keys)) for x in account_names]
+  df['Active']=(df[['Current Balance']] != 0).astype(int) # default zero accounts to inactive
+  df['Rlz share']=0
+  df['Unrlz share']=1
+  source_formulas=[ '=@[Account]','=@[Account] & " - TOTAL"' ]  #formula for (sub-accounts),  (leaves, and categories)
+  flag=pd.concat([df.total_type=='sub_account',df.Type!='I'],axis=1).all(axis=1)
+  df['Actl_source']=[source_formulas[ x] for x in flag]
+  source_tabs=['tbl_tranfers_actl','tbl_invest_actl']# actuals are sourced from this, (depends on account type)
+  df['Actl_source_tab']= [source_tabs[x=='I']for x in df.Type.tolist()]
+  df['Fcst_source']='zero' # a default, but should be changed by user
+  del df['total_type']
+  return df
