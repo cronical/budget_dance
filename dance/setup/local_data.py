9ad2216ed910043d725ec866afbd3be0dc64a6ab
George Dobbs <george.dobbs@gmail.com>
2022-07-08 11:15:55 -0400

    fix selector for actl_source

--- dance/setup/local_data.py
+++ dance/setup/local_data.py
@@ -1,6 +1,7 @@
 '''get annual various local data'''
 import pandas as pd
 from dance.util.files import tsv_to_df
+from dance.util.logs import get_logger
 
 def read_data(data_info):
   if data_info['type']== 'md_acct':
@@ -22,11 +23,20 @@ def filter_nz(df,include_zeros):
   nzza=pd.concat([nz, za],axis=1)
   return nzza.any(axis=1)
 
+def no_suffix(acct_total_key):
+  '''Drop the total sufix
+  args:
+    acct_total_key: string including " - Total" as a suffix
+  returns: same without the suffix
+  '''
+  return ' - '.join(acct_total_key.split(' - ')[:-1])
+
 # -----------------------------
 def read_accounts(data_info):
   '''Get the account data as specified in the data_info.
 
   Depending on the data_info may take items at a total level or leaf level.
+  All investment accounts are processed at the total level for each account.  Securities are dropped.
 
   args:
     data_info: a dictionary with the path of the source file, and three lists:
@@ -34,8 +44,10 @@ def read_accounts(data_info):
     2. no_details_for - identifies categories that are covered by total levels
     3. include_zeros - indicates accounts that are wanted even though they are zero balance (otherwise ignored)
 
-  returns: dataframe including 'Account', 'Type', 'Current Balance' and 'total_type'
+  returns: dataframe including 'Account', 'Type', 'Current Balance', is_total, and 'level'
   '''
+
+  logger=get_logger(__file__)
   # doing the same with data frame
   types={'Assets':'A','Bank Accounts':'B','Credit Cards':'C','Investment Accounts':'I','Liabilities':'L','Loans':'N'}
   groups=data_info['group']
@@ -45,7 +57,11 @@ def read_accounts(data_info):
   g_totals=[g+' - Total' for g in groups]
 
   df=tsv_to_df(data_info['path'],skiprows=3)
-  df=df.dropna(how='any',subset='Account')
+  logger.info('{} rows read'.format(len(df)))
+  df=df.dropna(how='any',subset='Account') # remove blank rows
+  df.reset_index(drop=True,inplace=True)
+  logger.info('{} non-blank rows'.format(len(df)))
+  df['Account']=df['Account'].str.strip()
 
   # establish category for each row
   df['category']=''
@@ -56,56 +72,125 @@ def read_accounts(data_info):
       df.loc[ix,'Notes']='' # to allow selecting securities later
     df.loc[ix,'category']=cat
 
-  #df=df.loc[~ df['Notes'].str.contains('@')] # remove securities / currencies (?)
-
   # build a boolean grid to track where each account is handled
-  df.reset_index(drop=True,inplace=True)
-  for col in ['ignore','by_total','keep']:
+  attr=['is_total','keep']
+  dispo=['is_heading','rollup','zero_rule','by_total','no_detail','extra_totals']
+  for col in attr+dispo:
+    # is_total - keeps track of totals
+    # keep - indicates the ones to keep
+    # The rest are dispositions
+    # is_heading - well, its a heading
+    # rollup - is when a total is selected
+    # non zero rule - is True the zero rule was applied
+    # by_total - is True if its handled by a total
+    # no_detail - is True if its excluded by config
+    # extra_totals - grand total, total of all investments and total of category in name conflict
     df[col]=False
 
-  # distinguish between category and sub-account totals to handle name conflicts
-  df['total_type']='leaf'
-  tot_rows=df['Account'].str.contains(' - Total')
-  i_tots=pd.concat([tot_rows,df['category']=='Investment Accounts'],axis=1).all(axis=1)
-  i_totals=list(set(df.loc[i_tots,'Account'])-{'Investment Accounts - Total'})
+  # set the extra totals flag
+  xt=[x+ ' - Total'for x in no_details_for]+['Total']
+  df.loc[df['Account'].isin(xt),'extra_totals']=True
+  logger.info('{} rows marked as extraneous totals'.format(len(xt)))
+
+  tot_rows=df['Account'].str.contains(' - Total') # all the total rows on the report
+  logger.info('{} totals identified'.format(tot_rows.sum()))
+
+  # flag the investment total rows except total for all investments.
+  i_tots=tot_rows & (df['category']=='Investment Accounts') & (df['Account'] !='Investment Accounts - Total')
+  i_totals=df.loc[i_tots,'Account'].tolist()
+  logger.info('{} investment accounts identified'.format(len(i_totals)))
+
+  # establish the account level by use of the headings and totals
+  df['level_change']=0
   for ix,row in df.loc[tot_rows,['Account']].iterrows():
     acct_total_key=row['Account']
     acct_key=' - '.join(acct_total_key.split(' - ')[:-1])
     # mark the heading and the total lines
     sel=df['Account'].apply(lambda x: x in [acct_key , acct_total_key])
-    if sum(sel)==2: # the normal case
-      df.loc[sel,'total_type']=['sub_account','category'][acct_key in types]
-    if sum(sel)==4: # name conflict between category and sub-account total
-      ixs= df.loc[sel].index.tolist()
-      for y in ixs:
-        df.loc[y,'total_type']=['sub_account','category'][y in [ixs[0],ixs[-1]]]
-      sel=pd.concat([sel,df['total_type']=='sub_account'],axis=1).all(axis=1) # only include the sub_account variant
-      assert sum(sel)==2, 'too many name conflicts on {}'.format(acct_key)
+    ixs= df.loc[sel].index.tolist()
+    for i,ix in enumerate(ixs):
+      delta=[1,-1][i>=len(ixs)/2]
+      df.loc[ix,'level_change']=delta
+      if delta==-1:
+        df.loc[ix,'is_total']=True
+  df['level']=df['level_change'].cumsum(axis=0)
+  del df['level_change']
+  logger.info('assigned account hierarchy levels. Max level is {}'.format(df.level.max(axis=0)))
 
-    # now that there is no name conflict, mark rows
+  #remove the suffix from totals
+  df.loc[tot_rows,'Account']=df.Account.apply(no_suffix)
+  df.loc[tot_rows,'is_total']=True
+
+  # work with the ranges between the headings and the totals
+  dups=[]
+  for ix,row in df.loc[tot_rows,['Account']].iterrows():
+    acct_key=row['Account']
+    if acct_key in dups:
+      continue
+    acct_total_key=acct_key + ' - Total'
+    sel=df['Account']== acct_key     # mark the heading and the total lines
+
+    if sum(sel)==4:# first resolve potential name conflict between category and sub-account total
+      # by removing the category instance of the name
+      logger.info('naming conflict identified on name: {}'.format(acct_key))
+      sa=(df.loc[sel,'level']==0) # flag the category total line
+      sal=list(sa.values)
+      df.loc[sa.index[0],'is_heading']=True
+      df.loc[sa.index[-1],'extra_totals']=True # mark disposition of the one we will ignore
+      logger.info('ignoring the one at level {}'.format(df.loc[sa.index[-1],'level']))
+      sal=sal[len(sa)//2:len(sa)]# the 2nd half of the list.
+      sal=list(reversed(sal))+sal # reflect across the midpoint so as to also mark the header
+      sel.loc[sa[sal].index]=False # turn them off
+      dups+=[acct_key]
+    assert sum(sel)==2, 'too many name conflicts on {}'.format(acct_key)
+
+    # now that there is no name conflict mark dispostion of the rows
     start,end=tuple(df.loc[sel,[]].index)
-    df.loc[[start],'ignore']=True # no further use for headings
-    ixs=range(start+1,end)
-    if acct_total_key in g_totals+i_totals:
+    df.loc[[start],'is_heading']=True # no further use for headings, just mark it for quality check
+    ixs=range(start+1,end) # all the items included in this total
+    if acct_total_key in g_totals+i_totals: # process all investment accounts and the configured groups
+      df.loc[ixs,'by_total']=True
       df.loc[ixs,'keep']=False # leaf nodes covered by a total
-      df.loc[end,'Account']=acct_key
-      df.loc[[end],'keep']=filter_nz(df.loc[[end]],include_zeros)
-      pass
-    else:
-      if acct_key not in no_details_for:
-        sel2=filter_nz(df.loc[ixs],include_zeros)
-        df.loc[ixs,'keep']=sel2# the leaf nodes we will keep unless we have a total at a higher level, in which case, they will later be overwritten
-        df.loc[end+1,'keep']=False # totals we are ignoring
+      nzr=filter_nz(df.loc[[end]],include_zeros) # keep this total per the non-zero rules
+      df.loc[[end],'zero_rule']=True
+      df.loc[[end],'rollup']=nzr
+      df.loc[[end],'keep']=nzr
+    else: # items not investments or configured groups
+      if acct_key in no_details_for: #specifically excluded by config
+        this_lev=df.loc[ixs,'level']==df.loc[start,'level'] # only go one level, deeper needs its own rule
+        not_tot=~df.loc[ixs,'is_total']
+        mark=not_tot & this_lev
+        df.loc[[x for x in ixs if mark.loc[x]],'no_detail']=True
+      else:
+        nzr=filter_nz(df.loc[ixs],include_zeros)
+        df.loc[ixs,'keep']=nzr# the leaf nodes we will keep unless we have a total at a higher level, in which case, they will later be overwritten
+        df.loc[ixs,'zero_rule']=True
+        df.loc[end,'is_total']=True
+        nzr=filter_nz(df.loc[[end]],include_zeros) # keep this total per the non-zero rules
+        df.loc[[end],'zero_rule']=True
+        df.loc[[end],'rollup']=nzr
+        df.loc[[end],'keep']=nzr
+
+  logger.info('Rows counts for each type of disposition:')
+  for col in dispo:
+    logger.info('{}: {}'.format(col,df[col].sum(axis=0)))
+
+  flag_tot=df[['is_heading','rollup','zero_rule','by_total','no_detail','extra_totals']].sum(axis=1)
+  if flag_tot.min()==0:
+    logger.warning('The following accounts were not disposed of properly')
+    for _,row in df.loc[flag_tot==0].iterrows():
+      logger.warning(row['Account'])
 
   df=df.loc[df.keep]
   df['Type']=[types[x] for x in df.category.tolist()]
-  return df[['Account','Type','Current Balance','total_type']]
+  logger.info('Returning {} rows'.format(len(df)))
+  return df[['Account','Type','Current Balance','is_total' ,'level']]
 
 def prepare_account_tab(data_info, in_df):
   '''Add in the added fields
   args:
     data_info: a dictionary describing the data (from the config.yaml file)
-    in_df: a dataframe with columns: Account, Type, Current Balance, and total_type
+    in_df: a dataframe with columns: Account, Type, Current Balance, is_total and level
 
   returns: a dataframe for the accounts tab
   '''
@@ -119,10 +204,11 @@ def prepare_account_tab(data_info, in_df):
   df['Rlz share']=0
   df['Unrlz share']=1
   source_formulas=[ '=@[Account]','=@[Account] & " - TOTAL"' ]  #formula for (sub-accounts),  (leaves, and categories)
-  flag=pd.concat([df.total_type=='sub_account',df.Type!='I'],axis=1).all(axis=1)
+  flag=(df.level>0) & (df.Type!='I') & (df.is_total)
   df['Actl_source']=[source_formulas[ x] for x in flag]
   source_tabs=['tbl_tranfers_actl','tbl_invest_actl']# actuals are sourced from this, (depends on account type)
   df['Actl_source_tab']= [source_tabs[x=='I']for x in df.Type.tolist()]
   df['Fcst_source']='zero' # a default, but should be changed by user
-  del df['total_type']
+  del df['is_total']
+  del df['level']
   return df
