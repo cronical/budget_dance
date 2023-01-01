2f1d7d7b078bd40365157eac43c52b5d7f57198c
George Dobbs <george.dobbs@gmail.com>
2022-12-31 20:08:55 -0500

    wip standard approach to retir_vals

---
+++ dance/retire_load.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+'''
+Reads data from "data/retire_template.tsv" to create the retire_vals table
+'''
+import argparse
+
+
+import pandas as pd
+
+from dance.util.files import tsv_to_df
+
+def prepare_retire(data_info):
+  '''setup the retir_vals dataframe'''
+  string_fields='Item,Election,Start Date,Anny Dur Yrs,Anny Rate'.split(',')
+  df=tsv_to_df(data_info['path'],skiprows=3,nan_is_zero=False,string_fields=string_fields)
+  df['Start Date']=pd.to_datetime(df['Start Date'])
+  df[['Type','Who','Firm']]=df.Item.str.split(' - ',expand=True)
+  return df
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description ='Prepares the retirement table from the input template')
+  parser.add_argument('--path','-p',default= 'data/retire_template.tsv',help='The path and name of the input file')
+  args=parser.parse_args()
+  prepare_retire(data_info={'path':args.path})
