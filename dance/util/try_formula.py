291a7b5b93213c931bec546d53f0a532128a71e6
George Dobbs <george.dobbs@gmail.com>
2023-06-11 15:51:26 -0400

    WIP testing legacy array support

--- dance/util/try_formula.py
+++ dance/util/try_formula.py
@@ -1,13 +1,25 @@
 '''Discover logic to set array formula'''
 
-from dance.util.tables import load_workbook
+from dance.util.tables import load_workbook, df_for_table_name
+from dance.util.xl_formulas import filter_parser,eval_criteria
 
 source='data/test_wb.xlsm'
 table_name='tbl_transfers_plan'
 ws_name='transfers_plan'
 
+ref_table='tbl_accounts'
+ref_ws='accounts'
+formula= '=FILTER(tbl_accounts[Account],(tbl_accounts[Active])*(tbl_accounts[No Distr Plan]))'
+
+ref_df=df_for_table_name(ref_table,workbook=source,data_only=True)
+criteria=filter_parser(formula)
+left=eval_criteria(criteria,ref_df)
+count=left.sum()
+pass
+
 wb = load_workbook(filename = source, read_only=False, keep_vba=True,data_only=True)
 ws=wb[ws_name]
+
 for i in range(3,25):
     ws['F'+str(i)]= 'cat'
 #wb.save(source)
@@ -17,7 +29,6 @@ c=ws['f3']
 wb = load_workbook(filename = source, read_only=False, keep_vba=True)
 ws=wb[ws_name]
 
-formula= '=FILTER(tbl_accounts[Account],tbl_accounts[Active]=1)'
 
 # seems to need the prefixes - otherwise excel complains and strips out formula
 formula=formula.replace('FILTER','_xlfn._xlws.FILTER')
