546ad42e036c75aaf1b3c37d9df90307c4922c62
George Dobbs <george.dobbs@gmail.com>
2023-06-18 17:06:41 -0400

    new type of formula definition

--- dance/util/xl_formulas.py
+++ dance/util/xl_formulas.py
@@ -1,7 +1,7 @@
 '''Utility programs that deal with Excel formulas'''
 import re
-from openpyxl.formula import Tokenizer
-from pandas import DataFrame
+
+from dance.util.tables import df_for_range
 
 def table_ref(formula):
   '''Allows user to specify formula using the short form of "a field in this row" by converting it
@@ -20,16 +20,20 @@ def table_ref(formula):
   result=re.sub(regex_row,r'[[#This Row],[\1]]',result)
   return result
 
-def apply_formulas(table_info,data,ffy,is_actl):
+def apply_formulas(table_info,data,ffy,is_actl,wb=None,table_map=None):
   '''insert actual or forecast formulas per config
   The config may give formula definitions in sections called actl_formulas, all_col_formulas and/or fcst_formulas.
   If it exists, *_formulas section contains the key_field to use to match the key from the rules.
-  The rules are a list of dictionaries that contain entries for 'key' and 'formula'.
+  The rules are a list of dictionaries of two types both of which contain a 'formula'
+    - contain an enumeration of the values to match, or
+    - more complex queries
 
   args: table_info portion of the config for this table
   data: a dataframe to modify
   ffy: the first forecast year as Ynnnn
   is_actl: True to apply the actual formulas, False to apply the forecast formulas
+  wb: the in-memory openpyxl workbook (needed for lookups against already established tables)
+  table_map: the table to worksheet map (used for lookups)
 
   returns: the possibly modified dataframe.
   '''
@@ -40,11 +44,29 @@ def apply_formulas(table_info,data,ffy,is_actl):
       continue
     rules+=table_info[section]
   for rule in rules:
-    base_field=rule['base_field']
-    matches=rule['matches']
-    if not isinstance(matches,list):
-      matches=[matches]
-    for ix, rw in data.loc[data[base_field].isin(matches)].iterrows(): # the rows that match this rule
+    if 'matches' in rule:
+      base_field=rule['base_field']
+      matches=rule['matches']
+      if not isinstance(matches,list):
+        matches=[matches]
+      selection=data[base_field].isin(matches)
+    else:
+      assert 'query' in rule,'neither matches or query is in the rule'
+      queries=rule['query']
+      selection = True
+      for query in queries:
+        assert query['compare_with'] == '=','Nonce error - only equality implemented'
+        values=data[query['field']]
+        if 'look_up' in query: # if its a lookup perform the lookup.  
+          look_up=query['look_up']
+          ref_table=look_up['table']
+          source_ws=wb[table_map[ref_table]] 
+          ref_df=df_for_range(worksheet=source_ws,range_ref=source_ws.tables[ref_table].ref) # The index is the 1st column in the table
+          index_on=data[look_up["index_on"]]
+          values=ref_df.loc[index_on,look_up['value_field']].reset_index(drop=True)# lookup with the index then discard it
+        next_sel=(values == query['compare_to'])
+        selection=selection & next_sel
+    for ix, rw in data.loc[selection].iterrows(): # the rows that match this rule
       selector=is_actl
       yix=0 # special handling if first year: allow it to be skipped such as for a start bal, or have its own value.
       first_item=None
@@ -74,7 +96,7 @@ def apply_formulas(table_info,data,ffy,is_actl):
             data.at[ix,col]=formula
   return data
 
-def actual_formulas(table_info,data,ffy):
+def actual_formulas(table_info,data,ffy,wb=None,table_map=None):
   '''insert actual formulas per config
   The config may give a section called actl_formulas.
   If it exists, actl_formulas contains the key_field to use to match the key from the rules.
@@ -84,10 +106,10 @@ def actual_formulas(table_info,data,ffy):
   data: a dataframe to modify
   ffy: the first forecast year as Ynnnn
   '''
-  data=apply_formulas(table_info,data,ffy,True)
+  data=apply_formulas(table_info,data,ffy,True,wb=wb,table_map=table_map)
   return data
 
-def forecast_formulas(table_info,data,ffy):
+def forecast_formulas(table_info,data,ffy,wb=None,table_map=None):
   '''insert forecast formulas per config
   The config may give a section called fcst_formulas.
   If it exists, fcst_formulas contains the key_field to use to match the key from the rules.
@@ -97,7 +119,7 @@ def forecast_formulas(table_info,data,ffy):
   data: a dataframe to modify
   ffy: the first forecast year as Ynnnn
   '''
-  data=apply_formulas(table_info,data,ffy,False)
+  data=apply_formulas(table_info,data,ffy,False,wb=wb,table_map=table_map)
   return data
 
 def dyno_fields(table_info,data):
@@ -134,133 +156,7 @@ def dyno_fields(table_info,data):
         data.at[ix,action['target_field']]=value
   return data
 
-def filter_parser(formula):
-  '''crude method to parse filter formula to be used to determine number of rows in the result
-  
-  Converts a formula of a excel dynamic array FILTER, possibly wrapped with a SORT to a list.
-  Returns the table name, field name selected, and the list:
-    The items on the even indices are 3 element lists - the last two which may be None:
-    field, a comparison operator, and compare to value
-  Items on the odd indicies are of length 1, inhabited by a join symbol (and/or)
-  
-  This is quite limited and is intended to provide only a work around for openpyxl's lack of dynamic array support.
-  For filters with more than one criteria, use parentheses around each criteria.
-
-  
-  '''
-
-  field_pat=re.compile('\[([\w ]+)\]')
-
-  table_pat=re.compile('tbl_[a-zA-Z0-9]+')
-  matches=table_pat.findall(formula)
-  assert 1==len(set(matches)),'Formula does not refer to exactly one table'
-  table_name=matches[0]
-
-  tok=Tokenizer(formula)
-  toks=[]
-  for t in tok.items:
-    toks.append({"value":t.value,"type":t.type,"subtype":t.subtype})
-  df= DataFrame(toks)
-
-  df['open']=(df.subtype=='OPEN')*1
-  df['close'] =((df.subtype=='CLOSE')*-1).shift(periods=1,fill_value=0)
-  df['cum']=(df.open+df.close).cumsum()
-  df['phrase_nums']=(df.cum != df.cum.shift(fill_value=0)).cumsum()
-
-  result=[]
-  in_filter=False
-  after_comma=False
-  display_field=None
-  for i in df.phrase_nums.unique():
-    phrase_parsed=[None,None,None]
-    phrase=df.loc[df.phrase_nums==i]
-    for _,row in phrase.iterrows():
-      if row.type == 'FUNC':
-        if row.value=='FILTER(':
-          in_filter=True
-      if row.subtype == 'CLOSE':
-        if in_filter:
-          if phrase_parsed[0] is not None:
-            result.append(phrase_parsed)
-          if row.type == 'FUNC':
-            in_filter=False # assume no functions internal to FILTER
-      if row.type == 'SEP':
-        after_comma=True
-      if row.type=='OPERAND':
-        if row.subtype=='RANGE':
-          matches=field_pat.search(row.value)
-          field=matches.group(1)
-          if after_comma:
-            phrase_parsed[0]=field
-          else:
-            display_field=field
-        else:
-          val=row.value
-          if row.subtype=='NUMBER':
-              val=int(row.value) # assumes no decimal places are given
-          if row.subtype=='TEXT':
-            val=val.replace('"','')
-          phrase_parsed[2]=val
-      if row.type == 'OPERATOR-INFIX':
-        if phrase.shape[0]!=1:
-          phrase_parsed[1]=row.value
-        else:
-          result.append([row.value]) # this is the and or or (+ or *)
-  return table_name, display_field, result
-
-def test_filter_parser():
-  cases=[
-      '=FILTER(tbl_accounts[Account],tbl_accounts[Active]*tbl_accounts[No Distr Plan])',
-      '=FILTER(tbl_accounts[Account],(tbl_accounts[Active])*(tbl_accounts[No Distr Plan]))',
-      '=FILTER(tbl_accounts[Account],(tbl_accounts[Active]=1)*(tbl_accounts[No Distr Plan]=1))',
-      '=FILTER(tbl_accounts[Account],tbl_accounts[Active])',
-      '=FILTER(tbl_accounts[Account],(tbl_accounts[Type]="I")+(tbl_accounts[Type]="B"))',
-      '=SORT(FILTER(tbl_accounts[Account],tbl_accounts[Active]))',
-      '=SORT(FILTER(tbl_accounts[Account],(tbl_accounts[Active])*(tbl_accounts[No Distr Plan])))',
-      '=FILTER(tbl_accounts[Account],tbl_accounts[Active]=1)',
-      '=FILTER(tbl_accounts[Account],(tbl_accounts[Active]=1))',
-      '=FILTER(tbl_accounts[Account],(tbl_accounts[Active]=1)*(tbl_accounts[No Distr Plan]=1))'
-  ]
-
-  for formula in cases:
-    print(formula)
-    table,field,parsed=filter_parser(formula)
-    print(table,field,parsed)
-    print ("")
-
-def eval_criteria(criteria,ref_df):
-    '''given criteria from filter_parser and the reference table
-    return a boolean series that selects the records defined by the criteria'''
-    left=None
-    op=None
-    for ix,criterion in enumerate(criteria):
-        if 0== ix %2:
-            field,infix,val=criterion
-            if infix is None: # default
-                infix='='
-                val=1
-            assert infix=='=', 'Nonce: only equality supported, not '+infix
-            sel=ref_df[field] == val
-            if left is None:
-                left=sel
-            if op is not None:
-                match op:
-                    case '+':
-                        left=left | sel
-                    case '*':
-                        left=left & sel
-        else:
-            assert len(criterion)==1, 'Expected just and or or, not ' + str(criterion)
-            op=criterion[0]
-            assert op in '+*', 'Expected + or * not ' + op            
-    return left
 
 
-def main():
-  test='[@AcctName],[@Type],[@Active]'
-  test='=@get_val("End Bal" &  [@AcctName],"tbl_balances",y_offset(this_col_name(),-1))'
-  lf=table_ref(test)
-  print(lf)
 
-if __name__ == '__main__':
-  test_filter_parser()
+
