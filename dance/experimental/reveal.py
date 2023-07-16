c7d7a863706db916bb1455003adf162f1e904be5
George Dobbs <george.dobbs@gmail.com>
2023-07-16 08:47:48 -0400

    hack -> reveal and doesn't rezip

--- dance/experimental/reveal.py
+++ dance/experimental/reveal.py
@@ -1,8 +1,9 @@
-'''Manually set cm attribute'''
+#! /usr/bin/env python
+'''Open up excel for file inspection'''
 from os.path import exists
 from shutil import rmtree
 import zipfile
-from dance.util.files import zip_up
+#from dance.util.files import zip_up
 
 filename='data/test_wb.xlsm'
 #filename='dance/experimental/Book1.xlsx'
@@ -11,6 +12,6 @@ if exists('./tmp'):
 with zipfile.ZipFile(filename, 'r') as z:
   z.extractall('./tmp/')
 print("edit files in ./tmp")
-pass
-zip_up(filename,'tmp')
-print("OK, try to open file")
+#pass
+#zip_up(filename,'tmp')
+#print("OK, try to open file")
