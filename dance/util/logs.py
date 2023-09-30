5aecc588226bf58fb0f569ec91fcc8e85015d8e8
George Dobbs <george.dobbs@gmail.com>
2023-09-30 13:51:48 -0400

    cli logging

--- dance/util/logs.py
+++ dance/util/logs.py
@@ -1,5 +1,6 @@
-'''Sets up logger'''
-
+#! /usr/bin/env python
+'''Sets up logger, or if called from command line logs argument'''
+import argparse
 import logging
 from os.path import sep
 
@@ -13,3 +14,27 @@ def get_logger(filename,level=logging.INFO):
   name=filename.split(sep)[-1]
   name=name.split('.')[0]
   return logging.getLogger(name)
+
+if __name__=="__main__":
+  logger=get_logger(__file__)
+  description ='Writes args to log as a single line, use -d, -e, -w for debug, error, warning'
+  parser = argparse.ArgumentParser(description=description)
+  parser.add_argument('text', nargs='*')
+  g=parser.add_mutually_exclusive_group()
+  g.add_argument('--debug','-d',action='store_true')
+  g.add_argument('--warning','-w',action='store_true')
+  g.add_argument('--error','-e',action='store_true')
+  args=parser.parse_args()
+  msg=' '.join(args.text)
+  t=args.debug+(2*args.warning)+(4*args.error)
+  match t:
+    case 0:
+      logger.info(msg)
+    case 1:
+      logger.setLevel(logging.DEBUG)
+      logger.debug(msg)
+    case 2:
+      logger.warning(msg)
+    case 4:
+      logger.error(msg)
+  
