342bcb3e9f1730aad884aa62070aea194bad9576
George Dobbs <george.dobbs@gmail.com>
2026-01-02 19:24:40 -0500

    move build data checks into util, clean up

--- dance/util/cross_check.py
+++ dance/util/cross_check.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-'''Tools for verifying the result of spreadsheet creation and hunting down problems
+"""Tools for verifying the result of spreadsheet creation and hunting down problems
 
   This requires that the workbook has been opened by Excel and saved.
-  In order to populate the results of the Excel calculations.'''
+  In order to populate the results of the Excel calculations."""
 import argparse
 import json
 import sys
@@ -13,30 +13,30 @@ import pandas as pd
 from dance.util.files import read_config
 from dance.util.logs import get_logger
 from dance.util.tables import df_for_table_name
-from tests.balance_check import balance_test_pairs
+from dance.util.balance_check import balance_test_pairs
 from tests.iande_check import iande_test_pairs
-from tests.helpers import get_row_set
+from dance.util.helpers import get_row_set
 
 config=read_config()
 logger=get_logger(__file__)
 
 class Tester:
-  '''Does the comparisons and keeps track of counts'''
+  """Does the comparisons and keeps track of counts"""
   def __init__(self):
     self.test_count=0
     self.stats={} # keys are the test group, values are two element lists: element count, success count
   def __str__(self):
     return f'a tester. {self.test_count} tests run'
   def get_stats(self):
-    '''Get a dictionary of stats'''
+    """Get a dictionary of stats"""
     return self.stats
 
   def run_test(self,test_group,expected,found,tolerance=0.5,ignore_years=[],title='') ->list:
-    '''under the heading of a test_group, compare expected and found for columns showing which items vary
+    """under the heading of a test_group, compare expected and found for columns showing which items vary
     expected and found are of type pd.Series with a name.
     By default look for exact match, but if tolerance provided then the difference should be less than or equal to the tolerance.
     Returns a list of column names (years) where the test failed.
-    '''
+    """
     if test_group in self.stats:
       group_stat=self.stats[test_group]
     else:
@@ -68,13 +68,13 @@ class Tester:
     return failed
 
 def heading(label,punc):
-  '''show a heading'''
+  """show a heading"""
   n=(72-len(label))//2
   msg= ((punc*n)+' '+label+' ' + (punc*n))
   logger.info(msg)
 
 def results(test_group,tester):
-  '''returns 0 if all tests pass, otherwise 100*ratio of successes to test count'''
+  """returns 0 if all tests pass, otherwise 100*ratio of successes to test count"""
   punc='-'
   stats=tester.get_stats()
   if test_group=='*':
@@ -98,7 +98,7 @@ def results(test_group,tester):
   return score
 
 def legend(table_name,filter,agg=None):
-  '''return a usable legend constructed from the table name and filter'''
+  """return a usable legend constructed from the table name and filter"""
   legend='_'.join(table_name.split('_')[1:])
   legend+='[%s]'%filter
   if agg is not None:
@@ -106,7 +106,7 @@ def legend(table_name,filter,agg=None):
   return legend
 
 def row_to_value(workbook,test_group,tester,table,row_name,row_values,tolerance=0,ignore=[],title=''):
-  '''
+  """
     args: workbook
           test_group
           tester
@@ -115,7 +115,7 @@ def row_to_value(workbook,test_group,tester,table,row_name,row_values,tolerance=
           row_values: a list of values
           tolerance
           ignore - list of Yyears to ignore
-  '''
+  """
   found=get_row_set(workbook,table,'index','index',in_list=[row_name]).squeeze()
   if found.shape[0]==0:
     raise ValueError("Nothing found in "+row_name)
@@ -130,7 +130,7 @@ def row_to_value(workbook,test_group,tester,table,row_name,row_values,tolerance=
   tester.run_test(test_group,expected,found,tolerance,title=title)
 
 def row_to_row(workbook,test_group,tester,table_lines,factors=None,title=''):
-  '''
+  """
   Helper to test that two single rows match OR
   if the filter gets more than one row, the sum of those rows (times the factor) is used.
   (the purpose of the factor is to allow for subtraction in the case of two lines)
@@ -139,12 +139,12 @@ def row_to_row(workbook,test_group,tester,table_lines,factors=None,title=''):
     args: workbook
           test_group
           tester
-          table_lines: a dict with table names as key and lines as values. 
-          The first item in the dict is the expected value 
+          table_lines: a dict with table names as key and lines as values.
+          The first item in the dict is the expected value
           The line may be a string - which will use a contains filter
           The line may be a list - which will use the in_list filter
           factor - a factor for each of expected and found. Its length should be the same as the number of rows to be selected.
-  '''
+  """
   values=[]
   dfs=[] 
   tables=[]
@@ -172,19 +172,19 @@ def row_to_row(workbook,test_group,tester,table_lines,factors=None,title=''):
     values.append(series)
   failed=tester.run_test(test_group,values[0],values[1],title=title)
   if len(failed):
-    spaces="    "
-    print (f"{spaces}Addition info")
-    print (f"{spaces}------ {tables[0]}")
-    print (indent(dfs[0][failed],spaces))
-    print (f"{spaces}------ {tables[1]}")
-    print(indent(dfs[1][failed],spaces))
+    # spaces="    "
+    # print (f"{spaces}Addition info")
+    # print (f"{spaces}------ {tables[0]}")
+    # print (indent(dfs[0][failed],spaces))
+    # print (f"{spaces}------ {tables[1]}")
+    # print(indent(dfs[1][failed],spaces))
     pass
 
 def indent(df,spaces):
   return spaces + df.to_string().replace("\n", "\n"+spaces)
 
 def verify(workbook=None,test_group='*'):
-  '''Various checks'''
+  """Various checks"""
   test_groups='iande,cross_check,balances,cash_flow,taxes'.split(',')
   if test_group!='*':
     test_groups=[test_group]
@@ -212,19 +212,19 @@ def verify(workbook=None,test_group='*'):
   if test_group in test_groups:
     heading(test_group,'-')
 
-# IRA distributions
+    # IRA distributions
     test_lines={
       'tbl_retir_vals':'IRA - ',
       'tbl_iande':'Income:J:Distributions:IRA - TOTAL'}
     row_to_row(workbook,test_group,tester,test_lines,title="IRA Distributions")
   
-# Pensions
+    # Pensions
     test_lines={
       'tbl_retir_vals':'DB - ',
       'tbl_iande':'Income:I:Retirement income:Pension - TOTAL'}
     row_to_row(workbook,test_group,tester,test_lines,title="Pensions")
 
-# bank interest
+    # bank interest
     test_lines={
       'tbl_balances':'Bank Accounts:Retain:Rlz Int/Gn',
       'tbl_iande': 'Income:I:Invest income:Int:Bank'
@@ -233,7 +233,7 @@ def verify(workbook=None,test_group='*'):
 
     df=df_for_table_name(table_name='tbl_invest_actl',data_only=True,workbook=workbook)
 
-# sheltered capgn-sales
+    # sheltered capgn-sales
     sel=(df.ValType=='Gains' ) & (df.Acct_txbl==0)
     keys=df.loc[sel].index.to_list()
     test_lines={
@@ -242,7 +242,7 @@ def verify(workbook=None,test_group='*'):
     }
     row_to_row(workbook,test_group,tester,test_lines,title="Non-txbl Gain from sales")
     
-# non sheltered capgn - sales
+    # non sheltered capgn - sales
     sel=(df.ValType=='Gains' ) & (df.Acct_txbl==1)
     keys=df.loc[sel].index.to_list()
     test_lines={
@@ -252,26 +252,31 @@ def verify(workbook=None,test_group='*'):
 
     row_to_row(workbook,test_group,tester,test_lines,title="Txbl gn from sales")
 
-# cap gains
+    # cap gains
     test_lines={
     'tbl_iande':['Income:I:Invest income:CapGn:Sales','Income:I:Invest income:CapGn:Shelt:Sales'],
     'tbl_invest_actl':'Gains'}
     row_to_row(workbook,test_group,tester,test_lines,title="All Cap Gns from sales")
 
-# txbl accounts invest income
+    # txbl accounts invest income
     sel=(df.ValType=='Income' ) & (df.Acct_txbl==1)
     keys=df.loc[sel].index.to_list()
     test_lines={
       'tbl_invest_actl': keys,
-      'tbl_iande': ['Income:I:Invest income:CapGn:Mut LT','Income:I:Invest income:CapGn:Mut ST','Income:I:Invest income:Div:Reg','Income:I:Invest income:Div:Tax-exempt',
-                    'Income:I:Invest income:Int:Defered-tax','Income:I:Invest income:Int:Reg','Income:I:Invest income:Int:Tax-exempt','Income:I:Invest income:Int:Tax-exempt Muni Accrued Int Paid']
+      'tbl_iande': ['Income:I:Invest income:CapGn:Mut LT','Income:I:Invest income:CapGn:Mut ST',
+                    'Income:I:Invest income:Div:Reg','Income:I:Invest income:Div:Tax-exempt',
+                    'Income:I:Invest income:Int:Defered-tax','Income:I:Invest income:Int:Reg',
+                    'Income:I:Invest income:Int:Tax-exempt','Income:I:Invest income:Int:Tax-exempt Muni Accrued Int Paid']
     }
     row_to_row(workbook,test_group,tester,test_lines,title="Txbl account invest income")
     pass
 
-# Non-txbl invest income
+    # Non-txbl invest income
     sel=(df.ValType=='Income' ) & (df.Acct_txbl==0)
     keys=df.loc[sel].index.to_list()
+    # keys.sort()
+    # for key in keys:print(key)
+
     test_lines={
       'tbl_invest_actl': keys,
       'tbl_iande': ['Income:I:Invest income:Int:Shelt','Income:I:Invest income:Div:Shelt','Income:I:Invest income:CapGn:Shelt:Distr']
@@ -297,7 +302,7 @@ def verify(workbook=None,test_group='*'):
     for hsa in hsas:
       ignore=[]
       if 'Devenir' in hsa or 'GBD - Fidelity' in hsa:
-        ignore=['Y2018']
+        pass #ignore=['Y2018']
       expected=aw.loc[hsa]
       table,filter,agg=('tbl_iande',hsa,'diff')
       hsa_rows=get_row_set(workbook,table,'index','index',contains=filter)
@@ -332,8 +337,6 @@ def verify(workbook=None,test_group='*'):
     found=get_row_set(workbook,table,'index','index',contains=line).squeeze()
     found.name=legend(table,line)
     expected=found * 0
-    expected[0]=-42.06
-    expected[1]=42.06
     expected.name='expected'
     tester.run_test(test_group,expected,found,title="Cash flow")
  # ========================================
